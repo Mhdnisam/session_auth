b2e8e32008e6b85fb148fd6308aeb1ce2ab11a30
Mhdnisam <mhdnisamm@gmail.com>
2025-11-03 13:02:18 +0530

    add signals

--- session_auth_app/apps.py
+++ session_auth_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SessionAuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'session_auth_app'
+
+    def ready(self):
+
+        import session_auth_app.signals
